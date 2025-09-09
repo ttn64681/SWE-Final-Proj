556d31a007d27141ab5323246638f91c8537aa06
ttn64681 <153382937+ttn64681@users.noreply.github.com>
2025-09-09 14:23:36 -0400

    Reorganize frontend structure with src/ folder

---
+++ frontend/src/components/common/UserMenu.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function UserMenu() {
+  return (
+    <div>
+      <h1>User Menu Icon</h1>
+      <h2>User Menu Dropdown Options after clicked</h2>
+    </div>
+  );
+}
