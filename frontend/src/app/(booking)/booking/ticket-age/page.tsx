37bb2f3c10c5c4ab326973be9d12f1b5b963fd72
hilton326 <hjd59728@uga.edu>
2025-09-14 16:05:13 -0400

    I keep messing up after here

--- frontend/src/app/(booking)/booking/ticket-age/page.tsx
+++ frontend/src/app/(booking)/booking/ticket-age/page.tsx
@@ -1,68 +1,22 @@
 import NavBar from "@/components/common/navBar/NavBar";
-import TicketCounter from "@/components/specific/booking/ticket-age/TicketCounter";
-// import TicketTotal from "@/components/specific/booking/ticket-age/TicketTotal";
+import TicketTable from "@/components/specific/booking/ticket-age/TicketTable";
 
 export default function TicketAgePage() {
-  return (
-    <div className="w-screen h-screen bg-black">
-        <NavBar />
+    // Placeholder constant- in a later version, this maximum will be carried over from the Seats page.
+    const reservedSeats = 5;
+    return (
+        <div className="w-screen h-screen bg-black">
+            <NavBar />
 
-        <div className="ml-8 mt-8">
-            <h1 className="text-4xl text-acm-pink"> Ticket Selection </h1>
-            <div className="mr-8 mt-2 border-1 border-gray-500"> </div>
-        </div>
+            <div className="ml-8 mt-8">
+                <h1 className="text-4xl text-acm-pink"> Ticket Selection </h1>
+                <div className="mr-8 mt-2 border-1 border-gray-500"> </div>
+            </div>
 
-        <h2 className="ml-8 mt-8 text-2xl text-white"> Choose tickets for each seat you reserved. </h2>
+            <h2 className="ml-8 mt-8 text-2xl text-white"> Choose tickets for each seat you reserved. </h2>
 
-        <div className="flex flex-col">
-            <table className="table-auto w-[50vw] h-[50vh] border-collapse border border-white mt-8 ml-8 text-white text-xl">
-                <tbody>
-                    {/* Labels */}
-                    <tr>
-                        <td className="p-8 text-3xl font-semibold"> Ticket</td>
-                        <td className="p-8 text-3xl font-semibold"> Quantity </td>
-                        <td className="p-8 text-3xl font-semibold"> Price</td>
-                        <td> </td>
-                    </tr>
-                    {/* Adult tickets */}
-                    <tr>
-                        <td className="p-8"> Adult</td>
-                        <td className=""> <TicketCounter /> </td>
-                        <td className="p-8"> $5.00</td>
-                        <td> </td>
-                    </tr>
-                    {/* Child tickets */}
-                    <tr>
-                        <td className="p-8"> Child</td>
-                        <td className=""> <TicketCounter/> </td>
-                        <td className="p-8"> $3.50</td>
-                        <td> </td>
-                    </tr>
-                    {/* Senior tickets */}
-                    <tr>
-                        <td className="p-8"> Senior</td>
-                        <td className=""> <TicketCounter /> </td>
-                        <td className="p-8"> $2.00</td>
-                        <td> </td>
-                    </tr>
-                    {/* Totals */}
-                    <tr>
-                        <td className="p-8 text-3xl font-semibold text-acm-pink"> Total:</td>
-                        <td className="p-8 text-3xl font-semibold text-acm-pink"> 0 </td>
-                        <td className="p-8 text-3xl font-semibold text-acm-pink"> $0.00</td>
-                        <td> 
-                            <button className="border-2 border-acm-pink rounded-xl text-white text-center text-2xl"> 
-                                <h1> Checkout  </h1>
-                             </button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-
-            
+            <TicketTable max={reservedSeats} />
 
         </div>
-
-    </div>
     );
 }
