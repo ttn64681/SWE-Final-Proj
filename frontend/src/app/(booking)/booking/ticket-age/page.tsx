8caf40b68902dd4a0e0da150975148c185d2720d
hilton326 <hjd59728@uga.edu>
2025-09-14 13:21:32 -0400

    9/14- Ticket counter buttons functional
    Next: Make the total tickets and price update based on the ticket category counters.

--- frontend/src/app/(booking)/booking/ticket-age/page.tsx
+++ frontend/src/app/(booking)/booking/ticket-age/page.tsx
@@ -1,5 +1,6 @@
 import NavBar from "@/components/common/navBar/NavBar";
-import TicketQuantity from "@/components/specific/booking/ticket-age/TicketQuantity";
+import TicketCounter from "@/components/specific/booking/ticket-age/TicketCounter";
+// import TicketTotal from "@/components/specific/booking/ticket-age/TicketTotal";
 
 export default function TicketAgePage() {
   return (
@@ -13,7 +14,7 @@ export default function TicketAgePage() {
 
         <h2 className="ml-8 mt-8 text-2xl text-white"> Choose tickets for each seat you reserved. </h2>
 
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col">
             <table className="table-auto w-[50vw] h-[50vh] border-collapse border border-white mt-8 ml-8 text-white text-xl">
                 <tbody>
                     {/* Labels */}
@@ -21,37 +22,44 @@ export default function TicketAgePage() {
                         <td className="p-8 text-3xl font-semibold"> Ticket</td>
                         <td className="p-8 text-3xl font-semibold"> Quantity </td>
                         <td className="p-8 text-3xl font-semibold"> Price</td>
+                        <td> </td>
                     </tr>
                     {/* Adult tickets */}
                     <tr>
                         <td className="p-8"> Adult</td>
-                        <td className="p-8"> <TicketQuantity /> </td>
+                        <td className=""> <TicketCounter /> </td>
                         <td className="p-8"> $5.00</td>
+                        <td> </td>
                     </tr>
                     {/* Child tickets */}
                     <tr>
                         <td className="p-8"> Child</td>
-                        <td className="p-8"> <TicketQuantity /> </td>
+                        <td className=""> <TicketCounter/> </td>
                         <td className="p-8"> $3.50</td>
+                        <td> </td>
                     </tr>
                     {/* Senior tickets */}
                     <tr>
                         <td className="p-8"> Senior</td>
-                        <td className="p-8"> <TicketQuantity /> </td>
+                        <td className=""> <TicketCounter /> </td>
                         <td className="p-8"> $2.00</td>
+                        <td> </td>
                     </tr>
                     {/* Totals */}
                     <tr>
                         <td className="p-8 text-3xl font-semibold text-acm-pink"> Total:</td>
-                        <td className="p-8 text-3xl font-semibold text-acm-pink"> $0.00 </td>
+                        <td className="p-8 text-3xl font-semibold text-acm-pink"> 0 </td>
                         <td className="p-8 text-3xl font-semibold text-acm-pink"> $0.00</td>
+                        <td> 
+                            <button className="border-2 border-acm-pink rounded-xl text-white text-center text-2xl"> 
+                                <h1> Checkout  </h1>
+                             </button>
+                        </td>
                     </tr>
                 </tbody>
             </table>
 
-            <button className="mt-10 border-2 border-acm-pink rounded-xl text-white text-center text-2xl"> 
-                <h1> Checkout  </h1>
-            </button>
+            
 
         </div>
 
