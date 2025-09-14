97ba891145c0f069f44853d1631a4faaa60bd9d3
Hilton Del Valle <hjd59728@uga.edu>
2025-09-14 11:09:16 -0400

    9/12/25
    Finished most of the  CSS; now focusing on the logic

--- frontend/src/app/(booking)/booking/ticket-age/page.tsx
+++ frontend/src/app/(booking)/booking/ticket-age/page.tsx
@@ -1,37 +1,60 @@
 import NavBar from "@/components/common/navBar/NavBar";
-import TicketCategory from "@/components/specific/booking/ticket-age/TicketCategory";
+import TicketQuantity from "@/components/specific/booking/ticket-age/TicketQuantity";
 
 export default function TicketAgePage() {
   return (
     <div className="w-screen h-screen bg-black">
         <NavBar />
+
         <div className="ml-8 mt-8">
             <h1 className="text-4xl text-acm-pink"> Ticket Selection </h1>
             <div className="mr-8 mt-2 border-1 border-gray-500"> </div>
         </div>
 
-    <h2 className="ml-8 mt-8 text-2xl text-white"> Choose tickets for each seat you reserved. </h2>
-        <table className="table-auto border-collapse border border-white mt-8 ml-8 text-white text-xl">
-            <tbody>
-                <tr>
-                    <td className="p-16"> Adult</td>
-                    <td className="p-16"> 0</td>
-                    <td className="p-16"> $5.00</td>
-                </tr>
-                <tr>
-                    <td className="p-16"> Child</td>
-                    <td className="p-16"> 0</td>
-                    <td className="p-16"> $3.50</td>
-                </tr>
-                <tr>
-                    <td className="p-16"> Senior</td>
-                    <td className="p-16"> 0</td>
-                    <td className="p-16"> $2.00</td>
-                </tr>
-            </tbody>
-        </table>
-    
-    <button> Continue </button>
+        <h2 className="ml-8 mt-8 text-2xl text-white"> Choose tickets for each seat you reserved. </h2>
+
+        <div className="flex flex-col items-center">
+            <table className="table-auto w-[50vw] h-[50vh] border-collapse border border-white mt-8 ml-8 text-white text-xl">
+                <tbody>
+                    {/* Labels */}
+                    <tr>
+                        <td className="p-8 text-3xl font-semibold"> Ticket</td>
+                        <td className="p-8 text-3xl font-semibold"> Quantity </td>
+                        <td className="p-8 text-3xl font-semibold"> Price</td>
+                    </tr>
+                    {/* Adult tickets */}
+                    <tr>
+                        <td className="p-8"> Adult</td>
+                        <td className="p-8"> <TicketQuantity /> </td>
+                        <td className="p-8"> $5.00</td>
+                    </tr>
+                    {/* Child tickets */}
+                    <tr>
+                        <td className="p-8"> Child</td>
+                        <td className="p-8"> <TicketQuantity /> </td>
+                        <td className="p-8"> $3.50</td>
+                    </tr>
+                    {/* Senior tickets */}
+                    <tr>
+                        <td className="p-8"> Senior</td>
+                        <td className="p-8"> <TicketQuantity /> </td>
+                        <td className="p-8"> $2.00</td>
+                    </tr>
+                    {/* Totals */}
+                    <tr>
+                        <td className="p-8 text-3xl font-semibold text-acm-pink"> Total:</td>
+                        <td className="p-8 text-3xl font-semibold text-acm-pink"> $0.00 </td>
+                        <td className="p-8 text-3xl font-semibold text-acm-pink"> $0.00</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <button className="mt-10 border-2 border-acm-pink rounded-xl text-white text-center text-2xl"> 
+                <h1> Checkout  </h1>
+            </button>
+
+        </div>
+
     </div>
     );
 }
