b4daa38e6620985dd346fcee642bb5b485d16afd
SidhantDash <sidhantdash126@gmail.com>
2025-09-25 17:32:51 -0400

    Finished filters button, in process of making working search feature

--- frontend/src/components/specific/movies/FiltersGenreButton.tsx
+++ frontend/src/components/specific/movies/FiltersGenreButton.tsx
@@ -2,12 +2,20 @@
 
 interface FiltersGenreButtonProps {
     genre: string;
+    selected: boolean;
+    onChange: () => void;
 }
 
-export default function FiltersGenreButton({ genre }: FiltersGenreButtonProps) {
+export default function FiltersGenreButton({ genre, selected, onChange }: FiltersGenreButtonProps) {
   return (
     <div className="inline-block">
-      <input id={genre} type="checkbox" className="hidden peer"/>
+      <input
+        id={genre}
+        type="checkbox"
+        className="hidden peer"
+        checked={selected}
+        onChange={onChange}
+      />
       <label
         htmlFor={genre}
         className="inline-flex px-4 py-2 border-2 border-acm-pink rounded-full cursor-pointer select-none transition duration-200 text-acm-pink peer-checked:bg-acm-pink peer-checked:text-black  font-bold"
