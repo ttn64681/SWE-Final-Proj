83b9d0958795162e4cf9e63025cc80b03ae832fc
SidhantDash <sidhantdash126@gmail.com>
2025-09-26 21:02:13 -0400

    Genre component update

--- frontend/src/components/common/Genre.tsx
+++ frontend/src/components/common/Genre.tsx
@@ -1,9 +1,13 @@
 
 
-export default function Genre(text: string) {
+interface GenreProps {
+    text: string;
+    imageUrl: string;
+}
+export default function Genre({text, imageUrl} : GenreProps) {
 
     return (
-        <div className="">
+        <div className="px-24 py-12 border-2 border-white/40 text-white">    
             <h3>{text}</h3>
         </div>
     );
