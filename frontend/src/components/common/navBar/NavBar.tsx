de03028ffd06817df5b4bf062fc9e7e785269771
Thai Nguyen <ttn64681@uga.edu>
2025-09-26 23:00:57 -0400

    minor setup and readme changes

--- frontend/src/components/common/navBar/NavBar.tsx
+++ frontend/src/components/common/navBar/NavBar.tsx
@@ -52,7 +52,7 @@ export default function NavBar() {
         </Link>
 
         {/* Hamburger Menu */}
-        <button className="text-white">
+        <button type="button" className="text-white" title="Hamburger Menu">
           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
           </svg>
