b4f633b7b626f21e64d2d830685b350d5199f82c
Thai Nguyen <ttn64681@uga.edu>
2025-10-09 16:00:47 -0400

    Added DB Backup Scripts + Refactored CorsConfig into SecurityConfig
    Added DB Backup Scripts + Refactored CorsConfig into SecurityConfig

--- backend/src/main/java/com/acm/cinema_ebkg_system/dto/RegisterRequest.java
+++ backend/src/main/java/com/acm/cinema_ebkg_system/dto/RegisterRequest.java
@@ -1,5 +1,12 @@
 package com.acm.cinema_ebkg_system.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data                    // Lombok: auto-generates getters, setters, toString, equals, hashCode
+@NoArgsConstructor      // Lombok: generates default constructor
+@AllArgsConstructor     // Lombok: generates constructor with all fields
 public class RegisterRequest {
     private String email;
     private String password;
@@ -9,84 +16,4 @@ public class RegisterRequest {
     private String address;
     private String state;
     private String country;
-
-    // Constructors
-    public RegisterRequest() {}
-
-    public RegisterRequest(String email, String password, String firstName, String lastName, 
-                          String phoneNumber, String address, String state, String country) {
-        this.email = email;
-        this.password = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phoneNumber = phoneNumber;
-        this.address = address;
-        this.state = state;
-        this.country = country;
-    }
-
-    // Getters and Setters
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
 }
