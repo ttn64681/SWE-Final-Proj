680ca0cf484d4345743fa073a1f9d318594ff3cf
SidhantDash <sidhantdash126@gmail.com>
2025-10-13 02:25:00 -0400

    Implemented GET/POST endpoints for payment info

---
+++ backend/src/main/java/com/acm/cinema_ebkg_system/dto/payment/PaymentRequest.java
@@ -0,0 +1,17 @@
+package com.acm.cinema_ebkg_system.dto.payment;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentRequest {
+    
+    private Long card_number;
+    private String billing_address;
+    private LocalDate expiration_date;    
+}
